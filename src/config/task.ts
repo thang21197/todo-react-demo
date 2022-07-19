8ecc2af1edd5ad0b36590923c7a9adeeab9429f6
thang21197 <48194900+thang21197@users.noreply.github.com>
2022-07-19 09:01:52 +0700

    Init

--- src/config/task.ts
+++ src/config/task.ts
@@ -2,9 +2,7 @@ import { ITodo } from "../store/todoStore";
 import { API } from "./api-config";
 
 const tasks = {
-  getAllTask: (params?: string) => {
-    API.get(`todos/my-todo?${params}`);
-  },
+  getAllTask: (params?: string) => API.get(`todos/my-todo?${params}`),
   addNewTask: (data: ITodo) => API.post(`todos`, data)
 };
 
